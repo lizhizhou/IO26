5914340100c81f444f97949c718d10f30645bdab
lizhizhou <lzz@meteroi.com>
2012-11-11 17:52:36 +0800

    fan_motor
    BETA

--- fan_motor.c
+++ fan_motor.c
@@ -5,3 +5,37 @@
  *      Author: lizhizhou
  */
 
+#include "fan_motor.h"
+#include "FPGA.h"
+
+#define FANMOTOR_CTRL_ADDRESS 0x2A0
+#define FANMOTOR_CTRL_BASE    (FANMOTOR_CTRL_ADDRESS/4)
+#define FANMOTOR_ID            *((volatile int*) FPGA_SYS_BASE+FANMOTOR_CTRL_BASE)
+#define FANMOTOR_PWM_FREQUENCE *((volatile int*) FPGA_SYS_BASE+FANMOTOR_CTRL_BASE+1)
+#define FANMOTOR_PWM_WIDTH     *((volatile int*) FPGA_SYS_BASE+FANMOTOR_CTRL_BASE+2)
+#define FANMOTOR_ON_OFF        *((volatile int*) FPGA_SYS_BASE+FANMOTOR_CTRL_BASE+3)
+
+const static int frequent   = 1000; // PWM frequence
+const static int duty_cycle = 50;   // PWM duty_cycle
+
+void fan_motor_init()
+{
+	FANMOTOR_PWM_FREQUENCE = frequent * 0x100000000 / 200000000;
+	FANMOTOR_PWM_WIDTH = 0xFFFFFFFF / 100 * duty_cycle;
+	FANMOTOR_ON_OFF = 1; 
+}
+
+void fan_ON()
+{
+	FANMOTOR_ON_OFF = 1; 
+}
+
+void fan_OFF()
+{
+	FANMOTOR_ON_OFF = 0; 
+}
+
+void fan_motor_set_pwm(unsigned int pwm)
+{
+	FANMOTOR_PWM_WIDTH = pwm;
+}
--- fan_motor.h
+++ fan_motor.h
@@ -7,6 +7,8 @@
 
 #ifndef FAN_MOTOR_H_
 #define FAN_MOTOR_H_
-
+extern void fan_motor_init();
+extern void fan_ON();
+extern void fan_motor_set_pwm(unsigned int pwm);
 
 #endif /* FAN_MOTOR_H_ */
