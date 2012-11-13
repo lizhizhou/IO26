86a108efef0fa68372d32252abbc4d743a1e4ecd
Zhizhou Li <lzz@meteroi.com>
2012-11-13 20:44:59 +0800

    test ok

--- Brush_motor.h
+++ Brush_motor.h
@@ -11,6 +11,8 @@
 extern void Brush_motor_init();
 extern void Brush_motor_forward();
 extern void Brush_motor_back();
+extern void Brush_motor_ON();
+extern void Brush_motor_OFF();
 extern void Brush_motor_set_pwm(unsigned int pwm);
 
 #endif /* BRUSHMOTOR_H_ */
--- main.c
+++ main.c
@@ -16,12 +16,22 @@ int main(int argn, char* argv[])
 	  ShieldCtrl_init();
 	  AM2301_init();
 	  Brush_motor_init();
-	  //Brush_motor_forward();
-	  Brush_motor_back();
+	  fan_motor_init();
 	  //IOA_OE      = 0xffffffff;
 	  //IOB_OE      = 0xffffffff;
 	  printf("Position A is %d\n",get_position_A());
 	  printf("Position B is %d\n",get_position_B());
+	  while(1) {
+		  Brush_motor_ON();
+		  fan_ON();
+		  Brush_motor_forward();
+		  getchar();
+		  Brush_motor_back();
+		  getchar();
+		  Brush_motor_OFF();
+		  fan_OFF();
+		  getchar();
+	  }
 	  fpga_close();
 	  //trace_back();
 	  return 0;
