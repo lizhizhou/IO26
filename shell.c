a4e21e0efe0bde143becf8786987b8a0c91fcc30
lizhizhou <lzz@meteroi.com>
2013-09-02 08:14:51 -0700

    bug fix for set_ref
    Signed-off-by: lizhizhou <lzz@meteroi.com>

--- shell.c
+++ shell.c
@@ -196,7 +196,18 @@ int microscop_ref(int argc,char* argv[])
     if(i == 4)
     {
     	micorscope_run_to_coordinates(o);
-    	micorscope_set_coordinates_zero();
+    	r[0].x=0;
+    	r[0].y=0;
+    	r[0].z=0;
+    	r[1].x=0;
+    	r[1].y=0;
+    	r[1].z=0;
+    	r[2].x=0;
+    	r[2].y=0;
+    	r[2].z=0;
+    	r[3].x=0;
+    	r[3].y=0;
+    	r[3].z=0;
     	return (true);
     }
     r[i] = micorscope_get_coordinates();
